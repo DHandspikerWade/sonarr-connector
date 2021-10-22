cdf958277ec4a4a96b3c6213ad93a5a776d5fe9e
Devin Handspiker-Wade <Devin@handspiker-wade.com>
2021-10-21 21:05:57 -0300

    Updated crontab to start new shell

--- run.sh
+++ run.sh
@@ -13,8 +13,9 @@ echo "" >> $HOME/.env
 touch /var/log/cron.log
 chmod +x /app/{nhk,youtube}.sh
 
-echo "0 5 * * * . $HOME/.env; /app/nhk.sh >> /var/log/cron.log 2>&1
-0 10 * * */3 . $HOME/.env; /app/youtube.sh >> /var/log/cron.log 2>&1
+# Start a another shell to work around crond's minimum environment. Just makes testing easier as cron would same env as running it manually
+echo "0 5 * * * sh -c '. $HOME/.env; /app/nhk.sh >> /var/log/cron.log 2>&1'
+0 10 * * */3 sh -c '. $HOME/.env; /app/youtube.sh >> /var/log/cron.log 2>&1'
 # This extra line makes it a valid cron" > /app/scheduler.txt
 crontab /app/scheduler.txt
 
